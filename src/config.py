9dcc68bc10d4c71752b1e58f8424ce894c5cdb0a
ronimos <41879450+ronimos@users.noreply.github.com>
2022-02-04 10:56:37 -0700

    Added option to read cnfg file.
    Added cnfg file to make changes in the datasets list

--- src/config.py
+++ src/config.py
@@ -1,12 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 29 16:39:01 2022
-
-@author: Ron Simenhois
-"""
+from configparser import ConfigParser
 
 DATA_PATH = '../data'
 LOG_PATH = '../LOGS'
 service_url: str = 'https://m2m.cr.usgs.gov/api/api/json/stable/'
 kml_file = 'AvalancheDetectionArea.kml'
-dataset_names = ['WORLDVIEW-1', 'WORLDVIEW-2', 'WORLDVIEW-3']
+config_file = 'cfng'
+config = ConfigParser()
+config.read(config_file)
+try: 
+    dataset_names = config['DATASETS']['dataset'].split()
+except KeyError:
+    dataset_names = ['WORLDVIEW-1', 'WORLDVIEW-2', 'WORLDVIEW-3']
